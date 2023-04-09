339e5819b1528ced3765a7d823cbf3e39a4f3d8d
江方舟 <826476841@qq.com>
2023-04-09 09:26:01 +0800

    [new files] more questions.

---
+++ NOWCODER_HJ25/src/main/java/Solution_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputs = "";
+        if (scanner.hasNextLine()) {
+            inputs = scanner.nextLine();
+        }
+        String rules = "";
+        if (scanner.hasNextLine()) {
+            rules = scanner.nextLine();
+        }
+
+    }
+}
